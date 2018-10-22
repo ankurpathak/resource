3d642c3a8b3873f541278b90a069203ac90fb6b4
Ankur Pathak <ankurpathak@live.in>
2018-10-22 12:26:59 +0530

    Second Part

---
+++ src/main/java/com/github/ankurpathak/resource/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.github.ankurpathak.resource;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().authorizeRequests()
+                .anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+    }
+
+}
---
+++ src/main/java/com/github/ankurpathak/resource/SessionConfig.java
@@ -0,0 +1,17 @@
+package com.github.ankurpathak.resource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
+import org.springframework.session.web.http.HttpSessionIdResolver;
+
+@Configuration
+public class SessionConfig  {
+
+    @Bean
+    public HttpSessionIdResolver httpSessionStrategy() {
+        return HeaderHttpSessionIdResolver.xAuthToken();
+    }
+
+
+}
